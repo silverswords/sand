5532858a5e72c8143d35071216f5a459c169ec40
flutterWang <wangbofeng980404@gmail.com>
2020-04-23 16:54:01 +0800

    modify pool struct

--- pkg/pool/pool.go
+++ pkg/pool/pool.go
@@ -3,52 +3,65 @@ package pool
 import (
 	"errors"
 	"sync"
-	"sync/atomic"
 )
 
 // Pool -
-type Pool interface {
-	Get() (interface{}, error)
-	Put(interface{}) error
-	Close() error
-}
-
-type pool struct {
-	m       sync.Mutex
-	wg      sync.WaitGroup
+type Pool struct {
+	lock    sync.Mutex
 	active  int64
 	max     int64
 	res     chan interface{}
 	isClose bool
-	closed  chan bool
+	close   chan bool
 }
 
 // New -
-func New(max int64) (*pool, error) {
+func New(max int64) (*Pool, error) {
 	if max <= 0 {
 		return nil, errors.New("size of the pool is too small")
 	}
 
-	pool := &pool{
+	pool := &Pool{
 		active:  0,
 		max:     max,
 		res:     make(chan interface{}, max),
-		closed:  make(chan bool),
+		close:   make(chan bool),
 		isClose: false,
 	}
+
+	go pool.start()
+
 	return pool, nil
 }
 
-func (p *pool) create() {
-	atomic.AddInt64(&p.active, 1)
-	obj := p.active
-	p.res <- obj
-	p.wg.Done()
+func (p *Pool) start() {
+	select {
+	case <-p.close:
+		p.isClose = true
+		for {
+			p.lock.Lock()
+			if int64(len(p.res)) == p.active {
+				close(p.res)
+				close(p.close)
+				p.lock.Unlock()
+				return
+			}
+			p.lock.Unlock()
+		}
+
+	}
 }
 
-func (p *pool) Get() (interface{}, error) {
-	p.m.Lock()
-	defer p.m.Unlock()
+func (p *Pool) create() interface{} {
+	p.active++
+	obj := p.active
+	return obj
+}
+
+// Get -
+func (p *Pool) Get() (interface{}, error) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
 
 	if p.isClose {
 		return nil, errors.New("this pool is closed")
@@ -59,21 +72,18 @@ func (p *pool) Get() (interface{}, error) {
 		return r, nil
 	default:
 		if p.active < p.max {
-			p.wg.Add(1)
-			go p.create()
-
-			select {
-			case r := <-p.res:
-				return r, nil
-			}
+			obj := p.create()
+			return obj, nil
 		}
-		return nil, errors.New("pool is empty")
 	}
+
+	return nil, errors.New("pool is empty")
 }
 
-func (p *pool) Put(obj interface{}) error {
-	p.m.Lock()
-	defer p.m.Unlock()
+// Put -
+func (p *Pool) Put(obj interface{}) error {
+	p.lock.Lock()
+	defer p.lock.Unlock()
 
 	select {
 	case p.res <- obj:
@@ -83,24 +93,8 @@ func (p *pool) Put(obj interface{}) error {
 	}
 }
 
-func (p *pool) wait() {
-	for {
-		if int64(len(p.res)) == p.active {
-			p.closed <- true
-			return
-		}
-	}
-}
-
-func (p *pool) Close() error {
-	p.isClose = true
-	p.wg.Wait()
-
-	go p.wait()
-
-	<-p.closed
-
-	close(p.res)
-	close(p.closed)
+// Close -
+func (p *Pool) Close() error {
+	p.close <- true
 	return nil
 }
