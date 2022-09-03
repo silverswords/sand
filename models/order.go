6ebdeb389813e1a69a27fe9eebec2aab03cc06c6
Wangying <82273284+Mqying@users.noreply.github.com>
2022-09-03 14:29:18 +0800

    fix order table and related functions

--- models/order.go
+++ models/order.go
@@ -19,33 +19,41 @@ const (
 var (
 	orderSQLString = []string{
 		fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (
-			order_id		VARCHAR(128) UNIQUE NOT NULL,
-			store_id		VARCHAR(128) UNIQUE NOT NULL,
-			pro_id			VARCHAR(128) UNIQUE NOT NULL,
-			open_id			VARCHAR(128) UNIQUE NOT NULL,
-			count			INT NOT NULL DEFAULT 1,
-			total_price 	DOUBLE NOT NULL,
-			status			INT NOT NULL DEFAULT 0,
-			created_time 	DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			order_id	 INT UNSIGNED NOT NULL AUTO_INCREMENT,
+			user_id		 BIGINT UNSIGNED NOT NULL,
+			pro_id 		 VARCHAR(50) NOT NULL,
+			store_id 	 VARCHAR(50) NOT NULL DEFAULT '000000',
+			quantity	 INT UNSIGNED NOT NULL,
+			total_price  DOUBLE UNSIGNED NOT NULL,
+			status 		 TINYINT UNSIGNED DEFAULT '0',
+			created_at 	 DATETIME DEFAULT NOW(),
 			PRIMARY KEY (order_id)
 		)  ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;`, OrderTableName),
-		fmt.Sprintf(`INSERT INTO %s (order_id, store_id, pro_id, open_id, count, total_price, status) VALUES (?, ?, ?, ?, ?, ?, ?)`, OrderTableName),
-		fmt.Sprintf(`SELECT order_id, pro_id, count, total_price, status FROM %s WHERE open_id = ?`, OrderTableName),
-		fmt.Sprintf(`SELECT order_id, pro_id, count, total_price, status FROM %s WHERE store_id = ?`, OrderTableName),
-		fmt.Sprintf(`SELECT order_id, store_id, pro_id, open_id, count, total_price, status FROM %s WHERE order_id = ?`, OrderTableName),
+		fmt.Sprintf(`INSERT INTO %s (order_id, user_id, pro_id, store_id, quantity, total_price, status) VALUES (?, ?, ?, ?, ?, ?, ?)`, OrderTableName),
+		fmt.Sprintf(`SELECT order_id, pro_id, quantity, total_price, status FROM %s WHERE open_id = ?`, OrderTableName),
+		fmt.Sprintf(`SELECT order_id, pro_id, quantity, total_price, status FROM %s WHERE store_id = ?`, OrderTableName),
+		fmt.Sprintf(`SELECT order_id, user_id, pro_id, store_id, quantity, total_price, status, create_at FROM %s WHERE order_id = ?`, OrderTableName),
 		fmt.Sprintf(`UPDATE %s SET status = ? WHERE order_id = ?`, OrderTableName),
 	}
 )
 
 type Order struct {
-	OrderID    string  `json:"order_id,omitempty"`
-	ProID      string  `json:"pro_id,omitempty"`
-	OpenID     string  `json:"open_id,omitempty"`
+	OrderID    uint32  `json:"order_id,omitempty"`
+	UserID     uint64  `json:"open_id,omitempty"`
+	ProductID  string  `json:"pro_id,omitempty"`
 	StoreID    string  `json:"store_id,omitempty"`
-	Count      uint64  `json:"count,omitempty"`
+	Quantity   uint32  `json:"count,omitempty"`
 	TotalPrice float64 `json:"total_price,omitempty"`
 	Status     uint8   `json:"status,omitempty"`
 	CreateTime string  `json:"create_time"`
+}
+
+type Item struct {
+	OrderID    uint32  `json:"order_id,omitempty"`
+	ProductID  string  `json:"pro_id,omitempty"`
+	Quantity   uint32  `json:"count,omitempty"`
+	TotalPrice float64 `json:"total_price,omitempty"`
+	Status     uint8   `json:"status,omitempty"`
 }
 
 // Create order table
@@ -59,9 +67,9 @@ func CreateOrderTable(db *sql.DB) error {
 }
 
 // Insert an order, get all info from admin
-func InsertOrder(db *sql.DB, order Order) error {
-	_, err := db.Exec(orderSQLString[mysqlInsertOrder], order.OrderID, order.StoreID,
-		order.ProID, order.OpenID, order.Count, order.TotalPrice, order.Status)
+func (order Order) InsertOrder(db *sql.DB) error {
+	_, err := db.Exec(orderSQLString[mysqlInsertOrder], order.OrderID, order.UserID,
+		order.ProductID, order.StoreID, order.Quantity, order.TotalPrice, order.Status)
 	if err != nil {
 		return err
 	}
@@ -70,29 +78,29 @@ func InsertOrder(db *sql.DB, order Order) error {
 }
 
 // Get brife order info by user's openID
-func GetOrderBrifeInfoByOpenID(db *sql.DB, openID string) ([]*Order, error) {
-	rows, err := db.Query(orderSQLString[mysqlOrderBrifeInfoByOpenID], openID)
+func ListOrderByUserID(db *sql.DB, userID uint64) ([]*Item, error) {
+	rows, err := db.Query(orderSQLString[mysqlOrderBrifeInfoByOpenID], userID)
 	if err != nil {
 		return nil, err
 	}
 
-	var result []*Order
+	var result []*Item
 	for rows.Next() {
 		var (
-			order_id    string
-			pro_id      string
-			count       uint64
+			order_id    uint32
+			product_id  string
+			quantity    uint32
 			total_price float64
 			status      uint8
 		)
-		if err := rows.Scan(&order_id, &pro_id, &count, &total_price, &status); err != nil {
+		if err := rows.Scan(&order_id, &product_id, &quantity, &total_price, &status); err != nil {
 			return nil, err
 		}
 
-		result = append(result, &Order{
+		result = append(result, &Item{
 			OrderID:    order_id,
-			ProID:      pro_id,
-			Count:      count,
+			ProductID:  product_id,
+			Quantity:   quantity,
 			TotalPrice: total_price,
 			Status:     status,
 		})
@@ -102,29 +110,29 @@ func GetOrderBrifeInfoByOpenID(db *sql.DB, openID string) ([]*Order, error) {
 }
 
 // Get brife order info by virtual store ID
-func GetOrderBrifeInfoByStoreID(db *sql.DB, storeID string) ([]*Order, error) {
+func ListOrderByStoreID(db *sql.DB, storeID string) ([]*Item, error) {
 	rows, err := db.Query(orderSQLString[mysqlOrderBrifeInfoByStoreID], storeID)
 	if err != nil {
 		return nil, err
 	}
 
-	var result []*Order
+	var result []*Item
 	for rows.Next() {
 		var (
-			order_id    string
-			pro_id      string
-			count       uint64
+			order_id    uint32
+			product_id  string
+			quantity    uint32
 			total_price float64
 			status      uint8
 		)
-		if err := rows.Scan(&order_id, &pro_id, &count, &total_price, &status); err != nil {
+		if err := rows.Scan(&order_id, &product_id, &quantity, &total_price, &status); err != nil {
 			return nil, err
 		}
 
-		result = append(result, &Order{
+		result = append(result, &Item{
 			OrderID:    order_id,
-			ProID:      pro_id,
-			Count:      count,
+			ProductID:  product_id,
+			Quantity:   quantity,
 			TotalPrice: total_price,
 			Status:     status,
 		})
@@ -135,33 +143,7 @@ func GetOrderBrifeInfoByStoreID(db *sql.DB, storeID string) ([]*Order, error) {
 
 // Get order detial info by orderID
 func GetOrderDetialByOrderID(db *sql.DB, orderID string) (*Order, error) {
-	var (
-		order_id    string
-		pro_id      string
-		open_id     string
-		store_id    string
-		count       uint64
-		total_price float64
-		status      uint8
-	)
-
-	err := db.QueryRow(orderSQLString[mysqlOrderDetialInfoByOrderID], orderID).Scan(&order_id,
-		&pro_id, &open_id, &store_id, &count, &total_price, &status)
-	if err != nil {
-		return nil, err
-	}
-
-	result := &Order{
-		OrderID:    orderID,
-		ProID:      pro_id,
-		OpenID:     open_id,
-		StoreID:    store_id,
-		Count:      count,
-		TotalPrice: total_price,
-		Status:     status,
-	}
-
-	return result, nil
+	return nil, nil
 }
 
 func ModifyOrderStatus(db *sql.DB, orderID string, status uint8) error {
