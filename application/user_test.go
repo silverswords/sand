4bf8abe11d94b97619bb6e999cb7d5e1f7d4b86c
Jundan Zhang <1750132640@qq.com>
2022-09-08 01:31:05 +0800

    add: user model and test for user

---
+++ application/user_test.go
@@ -0,0 +1,67 @@
+package application
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/silverswords/sand/model"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func init() {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=%t&loc=%s",
+		"root", "my-123456", "mqying.xyz", "3306", "mall", "utf8", true, "Local")
+
+	config := &Config{
+		Dsn: dsn,
+	}
+
+	instance := CreateApplication(config)
+	db = instance.GetDefaultGormDB()
+}
+
+func TestCreateUser(t *testing.T) {
+	if err := model.CreateUser(db, "1111", "1111", "12345678901"); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestQueryByUnionId(t *testing.T) {
+	id, err := model.QueryByUnionId(db, "1111")
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(id)
+}
+
+func TestQueryByMobile(t *testing.T) {
+	mobile := "12345678901"
+
+	user, err := model.QueryByMobile(db, mobile)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if user.UnionID != "1111" || user.Mobile != "12345678901" || user.OpenID != "1111" {
+		t.Error("query error")
+	}
+}
+
+func TestUpdateMobile(t *testing.T) {
+	mobile := "98765432109"
+	err := model.UpdateMobile(db, "1111", mobile)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	var expected string
+	db.Model(model.User{}).Select("mobile").Where(model.User{UnionID: "1111"}).Scan(&expected)
+	if expected != mobile {
+		t.Errorf("expected %s, got %s", expected, mobile)
+	}
+}
--- model/user.go
+++ model/user.go
@@ -1,7 +1,36 @@
 package model
 
+import "gorm.io/gorm"
+
 type User struct {
-	UnionID string
-	OpenID  string
-	Mobile  string
+	gorm.Model
+	UnionID string `gorm:"type:varchar(256);unique;not null"`
+	OpenID  string `gorm:"type:varchar(256);unique;not null"`
+	Mobile  string `gorm:"type:varchar(64) ;unique;not null"`
+}
+
+func CreateUser(db *gorm.DB, unionID string, openID string, mobile string) error {
+	return db.Model(User{}).Create(&User{UnionID: unionID, OpenID: openID, Mobile: mobile}).Error
+}
+
+func QueryByUnionId(db *gorm.DB, unionID string) (uint, error) {
+	var id uint
+	if err := db.Model(User{}).Select("id").Where("union_id = ?", unionID).Scan(&id).Error; err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func QueryByMobile(db *gorm.DB, mobile string) (*User, error) {
+	var user User
+	if err := db.Model(User{}).Where("mobile = ?", mobile).First(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func UpdateMobile(db *gorm.DB, unionID string, mobile string) error {
+	return db.Model(User{}).Where("union_id = ?", unionID).Update("mobile", mobile).Error
 }
