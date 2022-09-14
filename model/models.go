b5dcf90962b8647b4277560e52e60859db74cc8d
Wangying <82273284+Mqying@users.noreply.github.com>
2022-09-14 20:27:50 +0800

    fix: add category struct, fix errors of other structs in model.go

--- model/models.go
+++ model/models.go
@@ -14,26 +14,35 @@ type User struct {
 }
 
 type Order struct {
-	OrderID    string  `gorm:"type:varchar(64);unique;not null;primaryKey"`
+	gorm.Model
+	OrderID    string  `gorm:"type:varchar(64);unique;not null;primaryKey;"`
 	UserID     string  `gorm:"type:varchar(64);not null;"`
 	ProductID  string  `gorm:"type:varchar(64);not null;"`
 	StoreID    string  `gorm:"type:varchar(64);not null;"`
 	Quantity   uint32  `gorm:"not null"`
 	TotalPrice float64 `gorm:"type:float;precision:8;scale:2;not null"`
 	Status     uint8   `gorm:"not null"`
+}
+
+type Product struct {
+	ID         uint    `gorm:"primarykey"`
+	StoreID    uint    `gorm:"not null;default:0"`
+	CategoryID uint    `gorm:"not null"`
+	Price      float64 `gorm:"type:float;precision:8;scale:2;not null;default:9999.99"`
+	PhotoUrls  string  `gorm:"type:json"`
+	MainTitle  string  `gorm:"type:varchar(256)"`
+	Subtitle   string  `gorm:"type:varchar(256)"`
+	Status     uint8   `gorm:"not null;default:0"`
+	Stock      uint32  `gorm:"not null"`
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 }
 
-type Product struct {
-	ProductID string      `gorm:"type:varchar(64);unique;not null;primaryKey;"`
-	StoreID   string      `gorm:"type:varchar(64);not null;default:'0';"`
-	Price     float64     `gorm:"type:float;precision:8;scale:2;not null;default:9999.99;"`
-	MainTitle string      `gorm:"type:varchar(256);"`
-	Subtitle  string      `gorm:"type:varchar(256);"`
-	Images    interface{} `gorm:"type:json"`
-	Stock     uint32      `gorm:"not null"`
-	Status    uint8       `gorm:"not null;default:0;"`
+type Category struct {
+	ID        uint   `gorm:"primarykey"`
+	ParentID  uint   `gorm:"not null;default:0"`
+	Name      string `gorm:"not null;unique"`
+	Status    int8   `gorm:"not null;default:0"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
