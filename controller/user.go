ab8754be709b6e7d622e1fa1a8d5509cf1b91ed4
Jundan Zhang <1750132640@qq.com>
2022-09-24 01:43:51 +0800

    feature: add login method to userController

--- controller/user.go
+++ controller/user.go
@@ -6,13 +6,53 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/silverswords/sand"
+	"github.com/silverswords/sand/model"
 )
 
 type UserController struct {
 }
 
 func (c *UserController) RegisterRouter(r gin.IRouter) {
+	r.POST("/login", c.login)
 	r.GET("/info", c.getUserInfo)
+}
+
+func (c *UserController) login(ctx *gin.Context) {
+	var (
+		req struct {
+			Code string `json:"code"`
+		}
+		err error
+	)
+
+	err = ctx.ShouldBind(&req)
+	if err != nil {
+		ctx.Error(err)
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": http.StatusBadRequest})
+		return
+	}
+
+	loginResp, err := sand.GetApplication().Services().WeChat().Login(req.Code)
+	if err != nil {
+		ctx.Error(err)
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": http.StatusBadGateway})
+		return
+	}
+
+	_, err = sand.GetApplication().Services().Users().QueryByOpenID(loginResp.OpenID)
+	if err != nil {
+		ctx.Error(err)
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": http.StatusBadGateway})
+	}
+
+	user := &model.User{UnionID: loginResp.UnionID, OpenID: loginResp.OpenID}
+	err = sand.GetApplication().Services().Users().Create(user)
+	if err != nil {
+		ctx.Error(err)
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": http.StatusBadGateway})
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": http.StatusOK})
 }
 
 func (c *UserController) getUserInfo(ctx *gin.Context) {
