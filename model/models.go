85ea33b94faa6b65390aa3ba8902fbd152129fd1
Jundan Zhang <1750132640@qq.com>
2022-09-28 02:04:09 +0800

    fix: delete uselsee json tag

--- model/models.go
+++ model/models.go
@@ -3,9 +3,9 @@ package model
 import "time"
 
 type Model struct {
-	ID        uint64    `gorm:"primarykey" json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        uint64 `gorm:"primarykey" `
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
 type Category struct {
