354c64f347998a98db5b9c4d4c19ec7c1cc22128
Jundan Zhang <1750132640@qq.com>
2022-10-02 01:16:30 +0800

    fix: set http request header and signature to request

--- services/weChat.go
+++ services/weChat.go
@@ -7,11 +7,17 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"runtime"
+	"time"
+
+	"github.com/silverswords/sand/utils"
 )
 
 type config struct {
 	AppID     string `json:"app_id"`
 	AppSecret string `json:"app_secret"`
+	MchID     string `json:"mch_id"`
+	SerialNo  string `json:"serial_no"`
 }
 
 type LoginResponse struct {
@@ -85,6 +91,56 @@ func get(client *http.Client, url string) ([]byte, error) {
 	}
 
 	return buf, nil
+}
+
+func (s *weChat) request(method string, url string, jsonData []byte) (*http.Response, error) {
+	buf := bytes.NewBuffer(jsonData)
+
+	req, err := http.NewRequest(method, url, buf)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+
+	ua := fmt.Sprintf("api/v1(%s) Go/%s", runtime.GOOS, runtime.Version())
+	req.Header.Set("User-Agent", ua)
+
+	authorization, err := generateAuthorizationHeader(method, req.URL.RequestURI(), buf.String())
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", authorization)
+
+	resp, err := s.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func generateAuthorizationHeader(method, canonicalURL, signBody string) (string, error) {
+	config := getConfig()
+
+	nonce, err := utils.GenerateNonce()
+	if err != nil {
+		return "", err
+	}
+	timestamp := time.Now().Unix()
+	message := fmt.Sprintf("%s\n%s\n%d\n%s\n%s\n", method, canonicalURL, timestamp, nonce, signBody)
+	signature, err := utils.SignSHA256WithRSA(message)
+	if err != nil {
+		return "", err
+	}
+
+	authorization := fmt.Sprintf(
+		"%s mchid=\"%s\",nonce_str=\"%s\",timestamp=\"%d\",serial_no=\"%s\",signature=\"%s\"",
+		"Authorization: WECHATPAY2-SHA256-RSA2048", config.MchID,
+		nonce, timestamp, config.SerialNo, signature,
+	)
+	return authorization, nil
 }
 
 func getConfig() *config {
@@ -187,13 +243,13 @@ func (s *weChat) GetPhoneNumber(code string) (*PhoneResp, error) {
 	return &phoneResp, nil
 }
 
-func (s *weChat) GetPrepayID(description string, outTradeNo string, total int, openID string) (string, error) {
+func (s *weChat) GetPrepayInfo(description string, outTradeNo string, total int, openID string) (string, string, error) {
 	config := getConfig()
 
 	var (
 		postData struct {
 			AppID       string `json:"appid"`
-			Mchid       string `json:"mchid"`
+			MchID       string `json:"mchid"`
 			Description string `json:"description"`
 			OutTradeNo  string `json:"out_trade_no"`
 			NotifyUrl   string `json:"notify_url"`
@@ -210,6 +266,7 @@ func (s *weChat) GetPrepayID(description string, outTradeNo string, total int, o
 	)
 
 	postData.AppID = config.AppID
+	postData.MchID = config.MchID
 	postData.Description = description
 	postData.OutTradeNo = outTradeNo
 	postData.Amount.Total = total
@@ -218,29 +275,22 @@ func (s *weChat) GetPrepayID(description string, outTradeNo string, total int, o
 
 	jsonData, jsonErr := json.Marshal(postData)
 	if jsonErr != nil {
-		return "", jsonErr
+		return "", "", jsonErr
 	}
-
-	req, err := http.NewRequest(http.MethodPost, url[createOrder], bytes.NewBuffer(jsonData))
+	resp, err := s.request(http.MethodPost, url[createOrder], jsonData)
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
-
-	resp, err := s.client.Do(req)
-	if err != nil {
-		return "", err
-	}
-
 	defer resp.Body.Close()
 
 	buf, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
 
 	if err := json.Unmarshal(buf, &prepayResp); err != nil {
-		return "", err
+		return "", "", err
 	}
 
-	return prepayResp.PrepayID, nil
+	return prepayResp.PrepayID, config.AppID, nil
 }
